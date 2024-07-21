3171c450b154009ebdc37b19aaad21531e53e22d
Cethric <Cethric@users.noreply.github.com>
2024-07-21 12:05:02 +1000

    Add media service tests

---
+++ HomeScreen.Services/HomeScreen.Service.Media.UnitTests/Infrastructure/Location/Nominatim/NominatimLocationServiceUnitTest.cs
@@ -0,0 +1,134 @@
+﻿using System.Text;
+using FluentAssertions;
+using HomeScreen.Service.Media.Infrastructure.Location;
+using HomeScreen.Service.Media.Infrastructure.Location.NominatimLocationService;
+using HomeScreen.Service.Media.Infrastructure.Location.NominatimLocationService.Generated.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace HomeScreen.Service.Media.UnitTests.Infrastructure.Location.Nominatim;
+
+public class NominatimLocationServiceUnitTest
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(90, 90)]
+    public async Task GivenNominatimLocationService_WhenRequestingLocationFromCache_ThenReturnCachedString(
+        double lat,
+        double lon
+    )
+    {
+        // Arrange
+        var value = "Location Name";
+        var logger = new Mock<ILogger<NominatimLocationService>>();
+        var cache = new Mock<IDistributedCache>();
+        cache.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Encoding.UTF8.GetBytes(value));
+        var searchService = new Mock<INominatimClient>();
+        var service = new NominatimLocationService(logger.Object, cache.Object, searchService.Object);
+        // Act
+        var result = await service.SearchForLocation(lon, lat, 0, CancellationToken.None);
+        // Assert
+        result.Should().BeEquivalentTo(value);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(90, 90)]
+    public async Task
+        GivenNominatimLocationService_WhenRequestingLocationWithNoCacheAndInvalidResponse_ThenReturnUnknownLocation(
+            double lat,
+            double lon
+        )
+    {
+        // Arrange
+        var logger = new Mock<ILogger<NominatimLocationService>>();
+        var cache = new Mock<IDistributedCache>();
+        cache.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(null as byte[]);
+        var searchService = new Mock<INominatimClient>();
+        searchService.Setup(
+                x => x.Reverse_phpAsync(
+                    lat,
+                    lon,
+                    It.IsAny<OutputFormat>(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    It.IsAny<double>(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    It.IsAny<CancellationToken>()
+                )
+            )
+            .ReturnsAsync(
+                new SwaggerResponse<ReverseOutputJson>(
+                    404,
+                    new Dictionary<string, IEnumerable<string>>(),
+                    ReverseOutputJson.FromJson("{}")
+                )
+            );
+        var service = new NominatimLocationService(logger.Object, cache.Object, searchService.Object);
+        // Act
+        var result = await service.SearchForLocation(lon, lat, 0, CancellationToken.None);
+        // Assert
+        result.Should().BeEquivalentTo(ILocationService.UnknownLocation);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(90, 90)]
+    public async Task
+        GivenNominatimLocationService_WhenRequestingLocationWithNoCacheAndValidResponse_ThenReturnLocation(
+            double lat,
+            double lon
+        )
+    {
+        // Arrange
+        var location = "Example Location";
+        var logger = new Mock<ILogger<NominatimLocationService>>();
+        var cache = new Mock<IDistributedCache>();
+        cache.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(null as byte[]);
+        var searchService = new Mock<INominatimClient>();
+        searchService.Setup(
+                x => x.Reverse_phpAsync(
+                    lat,
+                    lon,
+                    It.IsAny<OutputFormat>(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    It.IsAny<double>(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    It.IsAny<CancellationToken>()
+                )
+            )
+            .ReturnsAsync(
+                new SwaggerResponse<ReverseOutputJson>(
+                    200,
+                    new Dictionary<string, IEnumerable<string>>(),
+                    ReverseOutputJson.FromJson($"{{\"display_name\": \"{location}\"}}")
+                )
+            );
+        var service = new NominatimLocationService(logger.Object, cache.Object, searchService.Object);
+        // Act
+        var result = await service.SearchForLocation(lon, lat, 0, CancellationToken.None);
+        // Assert
+        result.Should().BeEquivalentTo(location);
+    }
+}
