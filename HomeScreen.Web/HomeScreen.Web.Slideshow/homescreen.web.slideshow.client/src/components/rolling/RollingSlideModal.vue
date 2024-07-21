c1cede19d7882a1c1e69bead4a9645ef47659356
Cethric <Cethric@users.noreply.github.com>
2024-07-21 12:12:27 +1000

    Improve rolling image modal

--- HomeScreen.Web/HomeScreen.Web.Slideshow/homescreen.web.slideshow.client/src/components/rolling/RollingSlideModal.vue
+++ HomeScreen.Web/HomeScreen.Web.Slideshow/homescreen.web.slideshow.client/src/components/rolling/RollingSlideModal.vue
@@ -20,30 +20,37 @@
             },
           ]"
           :image="image"
-          :image-size="fullSize"
+          :image-size="imageSize"
           :load-image="loadImage"
-          :loading-size="loadingSize"
         />
       </div>
     </template>
 
     <template #default>
-      <PolaroidCard
-        :direction="Directions.horizontal"
-        :flat="true"
-        :image="image"
-        :load-image="loadImage"
-      >
-        <template #details="{ image }">
-          <component
-            :is="LeafletMapAsync"
-            v-if="image.location?.latitude && image.location?.longitude"
-            :latitude="image.location.latitude"
-            :longitude="image.location.longitude"
-            :tooltip="image.location.name"
-          />
+      <ModalDialog>
+        <template #activator="props">
+          <PolaroidCard
+            :direction="Directions.horizontal"
+            :flat="true"
+            :image="image"
+            :load-image="loadImage"
+            v-bind="props"
+          >
+            <template #details="{ image }">
+              <component
+                :is="LeafletMapAsync"
+                v-if="image.location?.latitude && image.location?.longitude"
+                :latitude="image.location.latitude"
+                :longitude="image.location.longitude"
+                :tooltip="image.location.name"
+              />
+            </template>
+          </PolaroidCard>
         </template>
-      </PolaroidCard>
+        <template #default>
+          <LargeImage :image="image" :load-image="loadImage" />
+        </template>
+      </ModalDialog>
     </template>
   </ModalDialog>
 </template>
@@ -53,36 +60,27 @@ import {
   type Direction,
   Directions,
   type Image,
-} from '@/helpers/component_properties';
-import PolaroidCard from '@components/PolaroidCard.vue';
-import ModalDialog from '@components/ModalDialog.vue';
-import { MediaTransformOptionsFormat } from '@/domain/api/homescreen-slideshow-api';
+  LeafletMapAsync,
+  type LoadImageCallback,
+  ModalDialog,
+  PolaroidCard,
+  ResponsiveImageSuspenseAsync,
+} from '@homescreen/web-components-client/src/index';
 import { useWindowSize } from '@vueuse/core';
-import { LeafletMapAsync } from '@/components/LeafletMapAsync';
 import { computed } from 'vue';
-import { ResponsiveImageSuspenseAsync } from '@/components/ResponsiveImageSuspenseAsync';
+import LargeImage from '@/components/LargeImage.vue';
 
 defineProps<{
   image: Image;
   direction: Direction;
-  loadImage: (
-    imageId: string,
-    width: number,
-    height: number,
-    blur: boolean,
-    format: MediaTransformOptionsFormat,
-  ) => Promise<string>;
+  loadImage: LoadImageCallback;
 }>();
 
 const emits = defineEmits<{ pause: []; resume: [] }>();
 
 const { width, height } = useWindowSize();
 
-const loadingSize = computed(() => ({
-  width: Math.max(Math.floor(width.value / 6), 200),
-  height: Math.max(Math.floor(height.value / 6), 200),
-}));
-const fullSize = computed(() => ({
+const imageSize = computed(() => ({
   width: Math.floor(width.value / 4),
   height: Math.floor(height.value / 4),
 }));
