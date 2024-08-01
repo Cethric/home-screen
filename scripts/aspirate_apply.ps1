ea0b94a6fb5f43f63d6fc987390efdd8c87a6f57
Cethric <Cethric@users.noreply.github.com>
2024-08-01 21:46:18 +1000

    Update gitversion config
    +semver: minor

---
+++ scripts/aspirate_apply.ps1
@@ -0,0 +1,3 @@
+﻿aspirate generate --image-pull-policy Always --container-image-tag latest --container-image-tag $(dotnet tool run dotnet-gitversion /output json /showvariable FullSemVer)
+
+aspirate apply
