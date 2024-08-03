52150ea2bcc98f5c1eaae3093b0158663245e411
Cethric <Cethric@users.noreply.github.com>
2024-08-04 01:00:56 +1000

    Memoize image loading

--- HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/src/components/ResponsiveImage.vue
+++ HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/src/components/ResponsiveImage.vue
@@ -1,10 +1,10 @@
 <template>
   <picture>
-    <source v-if="fullAvif && hasAvif" :srcset="fullAvif" type="image/avif" />
-    <source v-if="fullJxl && hasJxl" :srcset="fullJxl" type="image/jxl" />
-    <source v-if="fullWebP && hasWebP" :srcset="fullWebP" type="image/webp" />
-    <source v-if="fullPng && hasPng" :srcset="fullPng" type="image/png" />
-    <source v-if="fullJpeg && hasJpeg" :srcset="fullJpeg" type="image/jpeg" />
+    <source v-if="fullAvif" :srcset="fullAvif" type="image/avif" />
+    <source v-if="fullJxl" :srcset="fullJxl" type="image/jxl" />
+    <source v-if="fullWebP" :srcset="fullWebP" type="image/webp" />
+    <source v-if="fullPng" :srcset="fullPng" type="image/png" />
+    <source v-if="fullJpeg" :srcset="fullJpeg" type="image/jpeg" />
     <img
       :alt="
         image.location?.name ||
@@ -18,18 +18,12 @@
 
 <script async lang="ts" setup>
 import { type Image } from './properties';
-import type {
-  ComputedMediaSize,
-  LoadImageCallback,
+import {
+  type ComputedMediaSize,
+  type LoadImageCallback,
+  responsiveImageLoader,
 } from '../helpers/computedMedia';
-import { computedAsync, useMemoize } from '@vueuse/core';
-import Bowser from 'bowser';
-
-const hasFormatSupport = useMemoize((mime: string): boolean => {
-  const parser = Bowser.getParser(navigator.userAgent);
-  const isWebKit = /WebKit/.test(parser.getEngineName());
-  return isWebKit ? mime !== 'image/avif' : true;
-});
+import { useMemoize } from '@vueuse/core';
 
 const props = defineProps<{
   image: Image;
@@ -37,96 +31,60 @@ const props = defineProps<{
   imageSize: ComputedMediaSize;
 }>();
 
-const loading = await props.loadImage(
-  props.image.id,
-  Math.max(props.imageSize.width, 200),
-  Math.max(props.imageSize.height, 200),
-  true,
-  'Jpeg',
+const loadingImage = useMemoize(
+  async (imageId: string, width: number, height: number) =>
+    await props.loadImage(
+      imageId,
+      Math.max(width, 200),
+      Math.max(height, 200),
+      true,
+      'Jpeg',
+    ),
 );
 
-const hasAvif = hasFormatSupport('image/avif');
-const fullAvif = computedAsync(async (onCancel) => {
-  if (hasAvif) {
-    const abortController = new AbortController();
-    onCancel(() => abortController.abort('Component unload'));
-    return await props.loadImage(
-      props.image.id,
-      Math.max(props.imageSize.width, 200),
-      Math.max(props.imageSize.height, 200),
-      false,
-      'Avif',
-      abortController.signal,
-    );
-  }
-  return loading;
-}, loading);
-
-const hasJxl = hasFormatSupport('image/jxl');
-const fullJxl = computedAsync(async (onCancel) => {
-  if (hasJxl) {
-    const abortController = new AbortController();
-    onCancel(() => abortController.abort('Component unload'));
-    return await props.loadImage(
-      props.image.id,
-      Math.max(props.imageSize.width, 200),
-      Math.max(props.imageSize.height, 200),
-      false,
-      'JpegXL',
-      abortController.signal,
-    );
-  }
-  return loading;
-}, loading);
-
-const hasWebP = hasFormatSupport('image/webp');
-const fullWebP = computedAsync(async (onCancel) => {
-  if (hasWebP) {
-    const abortController = new AbortController();
-    onCancel(() => abortController.abort('Component unload'));
-    return await props.loadImage(
-      props.image.id,
-      Math.max(props.imageSize.width, 200),
-      Math.max(props.imageSize.height, 200),
-      false,
-      'WebP',
-      abortController.signal,
-    );
-  }
-  return loading;
-}, loading);
-
-const hasPng = hasFormatSupport('image/png');
-const fullPng = computedAsync(async (onCancel) => {
-  if (hasPng) {
-    const abortController = new AbortController();
-    onCancel(() => abortController.abort('Component unload'));
-    return await props.loadImage(
-      props.image.id,
-      Math.max(props.imageSize.width, 200),
-      Math.max(props.imageSize.height, 200),
-      false,
-      'Png',
-      abortController.signal,
-    );
-  }
-  return loading;
-}, loading);
-
-const hasJpeg = hasFormatSupport('image/jpeg');
-const fullJpeg = computedAsync(async (onCancel) => {
-  if (hasJpeg) {
-    const abortController = new AbortController();
-    onCancel(() => abortController.abort('Component unload'));
-    return await props.loadImage(
-      props.image.id,
-      Math.max(props.imageSize.width, 200),
-      Math.max(props.imageSize.height, 200),
-      false,
-      'Jpeg',
-      abortController.signal,
-    );
-  }
-  return loading;
-}, loading);
+const loading = await loadingImage(
+  props.image.id,
+  props.imageSize.width,
+  props.imageSize.height,
+);
+const fullAvif = responsiveImageLoader(
+  loading,
+  'Avif',
+  props.image.id,
+  props.imageSize.width,
+  props.imageSize.height,
+  props.loadImage,
+);
+const fullJxl = responsiveImageLoader(
+  loading,
+  'JpegXL',
+  props.image.id,
+  props.imageSize.width,
+  props.imageSize.height,
+  props.loadImage,
+);
+const fullWebP = responsiveImageLoader(
+  loading,
+  'WebP',
+  props.image.id,
+  props.imageSize.width,
+  props.imageSize.height,
+  props.loadImage,
+);
+const fullPng = responsiveImageLoader(
+  loading,
+  'Png',
+  props.image.id,
+  props.imageSize.width,
+  props.imageSize.height,
+  props.loadImage,
+);
+const fullJpeg = responsiveImageLoader(
+  loading,
+  'Jpeg',
+  props.image.id,
+  props.imageSize.width,
+  props.imageSize.height,
+  props.loadImage,
+);
 </script>
--- HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/src/helpers/computedMedia.ts
+++ HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/src/helpers/computedMedia.ts
@@ -1,13 +1,40 @@
+import {computedAsync, useMemoize} from "@vueuse/core";
+
 export interface ComputedMediaSize {
   width: number;
   height: number;
 }
+
+export type ImageFormat = 'Jpeg' | 'WebP' | 'Avif' | 'JpegXL' | 'Png';
 
 export type LoadImageCallback = (
   id: string,
   width: number,
   height: number,
   blur: boolean,
-  format: 'Jpeg' | 'WebP' | 'Avif' | 'JpegXL' | 'Png',
+  format: ImageFormat,
   signal?: AbortSignal,
 ) => Promise<string>;
+
+export const responsiveImageLoader = (
+    loading: string,
+    format: ImageFormat,
+    imageId: string,
+    width: number,
+    height: number,
+    loadImage: LoadImageCallback,
+) => {
+  const imageSrc = useMemoize(async () => {
+    return await loadImage(
+        imageId,
+        Math.max(width, 200),
+        Math.max(height, 200),
+        false,
+        format,
+    );
+  });
+
+  return computedAsync(async () => {
+    return await imageSrc();
+  }, loading);
+};
