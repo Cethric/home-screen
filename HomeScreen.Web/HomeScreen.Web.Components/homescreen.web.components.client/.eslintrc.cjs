50f248b05000b20782ac500c0a5cf885aa5b644f
Cethric <Cethric@users.noreply.github.com>
2024-08-11 17:28:35 +1000

    Upgrade packages
    +semver: minor

--- HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/.eslintrc.cjs
+++ HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/.eslintrc.cjs
@@ -1,17 +1,18 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "prettier",
-    "plugin:storybook/recommended",
-    "plugin:tailwindcss/recommended"
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'prettier',
+    'plugin:storybook/recommended',
+    'plugin:tailwindcss/recommended',
   ],
   parserOptions: {
-    ecmaVersion: "latest"
-  }
+    ecmaVersion: 'latest',
+  },
 };
--- HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/vite.config.ts
+++ HomeScreen.Web/HomeScreen.Web.Components/homescreen.web.components.client/vite.config.ts
@@ -4,9 +4,16 @@ import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    plugins: [vue(), dts()],
+    plugins: [
+      vue(),
+      dts({
+        tsconfigPath: fileURLToPath(
+          new URL('./tsconfig.app.json', import.meta.url),
+        )
+      }),
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
