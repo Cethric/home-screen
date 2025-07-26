74867fea72d14328af0c276677726098e632d8dc
Cethric <cethric@users.noreply.github.com>
2025-07-26 12:46:53 +1000

    chore: Update build pipeline

--- HomeScreen.Web/HomeScreen.Web.Common/homescreen.web.common.components/vite.config.ts
+++ HomeScreen.Web/HomeScreen.Web.Common/homescreen.web.common.components/vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig(() => {
 				formats: ["es"],
 				name: "web-common-components",
 				cssFileName: "web-common-components",
-                fileName: 'index.js',
+                fileName: 'index',
 			},
 			rollupOptions: {
 				external: ["vue", /@vueuse\/.*/, "luxon", /@fortawesome\/.*/, "zod"],
